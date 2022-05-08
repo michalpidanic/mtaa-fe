aecea1feed82f72fb30423045e09ccfb471c36cc
Ondrej Ambrus <ondro.ambrus@gmail.com>
2022-05-08 20:08:07 +0200

    working on message send

--- src/screens/ChatRoom.tsx
+++ src/screens/ChatRoom.tsx
@@ -7,34 +7,80 @@ import { useAuth } from '../contexts/AuthContext';
 import API from "../../Api";
 import Message from '../components/Message';
 import { Ionicons } from "@expo/vector-icons";
+import NetInfo from '@react-native-community/netinfo';
+import { State } from 'react-native-gesture-handler';
 
 export default function ChatsRoom() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const auth = useAuth();
     const [message, onChangeText] = useState('');
+    const [hasInternet, setConnectionStatus] = useState(false)
+    const [offlineList, refreshOfflineList] = useState([])
     
     const route = useRoute();
 
+    function refreshAll()
+    {
+        const listLength = offlineList.length;
+        for (let i = 0; i < listLength; i++)
+        {
+            API.post(offlineList[i]['endpoint'], offlineList[i]['body'], offlineList[i]['head'])
+            console.log(offlineList[i]['body'])
+        }
+        refreshOfflineList([])
+    }
+
+    function appendOfflineList(item)
+    {
+        const newList = offlineList.concat(item)
+        refreshOfflineList(newList)
+        console.log(offlineList)
+    }
+
+    function fetchNetworkStatus()
+    {
+        NetInfo.fetch().then(state => {
+            setConnectionStatus(state.isInternetReachable)
+        });
+    }
+
     const textSend = async () => {
+        let send = {}
         {message && auth.getAccessToken().then((token) => {
-            API.post('/message/send',
-            {
-                text: message,
-                mentions: "",
-                chatId: route.params.id,
-            },
-            {
-                headers: 
-                {
-                    Authorization: `Bearer ${token}`
-                }
+            send = {
+                endpoint: '/message/send',
+                body: {
+                        text: message,
+                        mentions: "",
+                        chatId: route.params.id,
+                    },
+                head: {
+                        headers: 
+                        {
+                            Authorization: `Bearer ${token}`
+                        }
+                    }
             }
-            )})
+            if (hasInternet == false)
+            {
+                API.post(send['endpoint'], send['body'], send['head'])
+            } else
+            {
+                appendOfflineList(send)
+            }
+        })
         }
     };
 
+    function checkInternetAndSend()
+    {
+        fetchNetworkStatus()
+        textSend()
+    }
+
     useEffect(() => {
+        fetchNetworkStatus()
         const fetchData = async () => {
         auth.getAccessToken().then((token) => {
             API.get(`chat/${route.params.id}/messages`, {
@@ -59,7 +105,10 @@ export default function ChatsRoom() {
     return (
         <SafeAreaView>
         <Appbar.Header style={styles.header}>
-          <Appbar.Content title={route.params.name} style={styles.header} />
+            <Appbar.Content title={route.params.name} style={styles.header} />
+            <TouchableOpacity onPress={refreshAll}>
+                {message ? <Ionicons style={styles.sendIcon} name={"refresh-circle"} size={30} color={"#ffffff"}/>:<Ionicons style={styles.sendIcon} name={"refresh-circle-outline"} size={30} color={"#ffffff"}/>}
+            </TouchableOpacity>
         </Appbar.Header>
         {loading && <Loading />}
         {!loading && (
@@ -81,7 +130,7 @@ export default function ChatsRoom() {
                         onChangeText={onChangeText}
                         value={message}
                     />
-                    <TouchableOpacity onPress={textSend}>
+                    <TouchableOpacity onPress={checkInternetAndSend}>
                         {message ? <Ionicons style={styles.sendIcon} name={"paper-plane"} size={30} color={"#ffffff"}/>:<Ionicons style={styles.sendIcon} name={"paper-plane-outline"} size={30} color={"#ffffff"}/>}
                     </TouchableOpacity>
                 </View>
